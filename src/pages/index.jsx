a9bd49b267c6abc04ec173fbc72cb92733d9f14a
Vitor Carmo <vitorv0071@gmail.com>
2022-01-22 21:20:44 -0300

    now with dynamic pages

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,14 +2,17 @@ import { useRef, useState } from "react";
 import { Container } from "../styles/pages/home";
 
 export default function Home() {
+  const pages = ["", "", "", "", "", ""];
+
   const [currentLocation, setCurrentLocation] = useState(1);
   const [numOfPapers, setNumOfPapers] = useState(3);
 
-  let maxLocation = numOfPapers + 1;
+  let maxLocation = pages.length + 1;
 
   const book = useRef(null);
   const prevBtn = useRef(null);
   const nextBtn = useRef(null);
+  const pageRef = useRef(null);
 
   const paper1 = useRef(null);
   const paper2 = useRef(null);
@@ -33,48 +36,35 @@ export default function Home() {
   };
 
   const goNextPage = () => {
+    const pageRef = document.getElementById(`paper-${currentLocation}`);
     if (currentLocation < maxLocation) {
-      switch (currentLocation) {
-        case 1:
-          openBook();
-          paper1.current.classList.add("flipped");
-          paper1.current.style.zIndex = 1;
-          break;
-        case 2:
-          paper2.current.classList.add("flipped");
-          paper2.current.style.zIndex = 2;
-          break;
-        case 3:
-          paper3.current.classList.add("flipped");
-          paper3.current.style.zIndex = 3;
-          closeBook(false);
-          break;
-        default:
-          throw new Error("unkown state");
+      if (currentLocation == 1) {
+        openBook();
       }
+      pageRef.classList.add("flipped");
+      pageRef.style.zIndex = currentLocation;
+
+      if (currentLocation === pages.length) {
+        closeBook(false);
+      }
+
       setCurrentLocation((currentLocation) => currentLocation + 1);
     }
   };
 
   const goPrevPage = () => {
+    const pageRef = document.getElementById(`paper-${currentLocation - 1}`);
+
     if (currentLocation > 1) {
-      switch (currentLocation) {
-        case 2:
-          closeBook(true);
-          paper1.current.classList.remove("flipped");
-          paper1.current.style.zIndex = 3;
-          break;
-        case 3:
-          paper2.current.classList.remove("flipped");
-          paper2.current.style.zIndex = 2;
-          break;
-        case 4:
-          openBook();
-          paper3.current.classList.remove("flipped");
-          paper3.current.style.zIndex = 1;
-          break;
-        default:
-          throw new Error("unkown state");
+      if (currentLocation === 2) {
+        closeBook(true);
+      }
+
+      pageRef.classList.remove("flipped");
+      pageRef.style.zIndex = pages.length - (currentLocation - 1);
+
+      if (pages.length === currentLocation - 1) {
+        openBook();
       }
 
       setCurrentLocation((currentLocation) => currentLocation - 1);
@@ -84,52 +74,35 @@ export default function Home() {
   return (
     <Container>
       <button ref={prevBtn} id="prev-btn" onClick={goPrevPage}>
-        <i className="fas fa-arrow-circle-left"></i>
+        <i className="fas fa-arrow-circle-left" />
       </button>
 
       <div id="book" ref={book} className="book">
-        <div id="p1" ref={paper1} className="paper">
-          <div className="front">
-            <div id="f1" className="front-content">
-              <h1>Front 1</h1>
+        {pages.map((page, index) => (
+          <div
+            style={{
+              zIndex: pages.length - index,
+            }}
+            className="paper"
+            id={`paper-${index + 1}`}
+            key={index}
+          >
+            <div className="front">
+              <div id="f1" className="front-content">
+                <h1>Front {index + 1}</h1>
+              </div>
+            </div>
+            <div className="back">
+              <div id="b1" className="back-content">
+                <h1>Back {index + 1}</h1>
+              </div>
             </div>
           </div>
-          <div className="back">
-            <div id="b1" className="back-content">
-              <h1>Back 1</h1>
-            </div>
-          </div>
-        </div>
-
-        <div id="p2" ref={paper2} className="paper">
-          <div className="front">
-            <div id="f2" className="front-content">
-              <h1>Front 2</h1>
-            </div>
-          </div>
-          <div className="back">
-            <div id="b2" className="back-content">
-              <h1>Back 2</h1>
-            </div>
-          </div>
-        </div>
-
-        <div id="p3" ref={paper3} className="paper">
-          <div className="front">
-            <div id="f3" className="front-content">
-              <h1>Front 3</h1>
-            </div>
-          </div>
-          <div className="back">
-            <div id="b3" className="back-content">
-              <h1>Back 3</h1>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
 
       <button ref={nextBtn} id="next-btn" onClick={goNextPage}>
-        <i className="fas fa-arrow-circle-right"></i>
+        <i className="fas fa-arrow-circle-right" />
       </button>
     </Container>
   );
